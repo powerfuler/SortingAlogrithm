474091881f1cf83dfdb3bb747a874ffd4ba9872b
dingjianmin <jm.ding@zuolin.com>
2021-04-08 18:31:19 +0800

    冒泡排序优化

--- Sorting Alogrithm/sorting/BubbleSort.java
+++ Sorting Alogrithm/sorting/BubbleSort.java
@@ -1,26 +1,32 @@
 package sorting;
 
+/**
+ * 冒泡排序
+ * 从小到大
+ */
 public class BubbleSort {
+
 	/**
-	 * 冒泡排序 
-	 * 从小到大
+	 * 一般排序
 	 * @param nums
 	 */
 	public static void bubbleSort(int[] nums) {
 		int n = nums.length;
 		for (int i = 0; i < n - 1; i++) {
 			for (int j = 0; j < n - i - 1; j++) {
+				// 位置交换
 				if (nums[j] > nums[j + 1]) {
 					int temp = nums[j];
 					nums[j] = nums[j + 1];
 					nums[j + 1] = temp;
 				}
 			}
+            printArray(i, nums);
 		}
 	}
 
 	/**
-	 * 改进的冒泡排序 
+	 * 改进的冒泡排序
 	 * 设置flag标志，判断排序是否提前结束 避免运行过多次数，优化代码
 	 * @param nums
 	 */
@@ -33,21 +39,63 @@ public class BubbleSort {
 					int temp = nums[j];
 					nums[j] = nums[j + 1];
 					nums[j + 1] = temp;
-					flag = 1;// 进行了元素交换的标志
+					// 进行了元素交换的标志
+					flag = 1;
 				}
 			}
 			if (flag == 0) {
 				break;
 			}
+            printArray(i, nums);
+		}
+	}
+
+	/**
+	 * 改进的冒泡排序
+	 * 设置flag标志，判断排序是否提前结束 避免运行过多次数，优化代码
+	 * 记录最后一次的交换边界,减少了交换次数
+	 * @param array
+	 */
+	public static void sort(int[] array) {
+		//记录最后一次交换的位置
+		int lastExchangeIndex = 0;
+		//无序数列的边界，每次比较只需要比到这里为止
+		int sortBorder = array.length - 1;
+		for (int i = 0; i < array.length; i++) {
+			//有序标记，每一轮的初始是true
+			boolean isSorted = true;
+			for (int j = 0; j < sortBorder; j++) {
+				if (array[j] > array[j + 1]) {
+					int tmp = array[j];
+					array[j] = array[j + 1];
+					array[j + 1] = tmp;
+					// 有元素交换，所以不是有序，标记变为false
+					isSorted = false;
+					// 把无序数列的边界更新为最后一次交换元素的位置
+					lastExchangeIndex = j;
+				}
+			}
+			sortBorder = lastExchangeIndex;
+			if (isSorted) {
+				break;
+			}
+            printArray(i, array);
 		}
 	}
 
 	public static void main(String[] args) {
-		int nums[] = { 8, 5, 2, 6, 9, 3, 1, 4, 0, 7 };
-		// sorting.BubbleSort(nums);
-		BubbleSortImprove(nums);
+		int nums[] = {8, 5, 2, 6, 9, 3, 1, 4, 0, 7, 11, 12, 13};
+//		bubbleSort(nums);
+//      BubbleSortImprove(nums);
+		sort(nums);
+	}
+
+	private static void printArray(int j, int[] nums) {
+		System.out.print("第" + (j + 1) + "轮比较，序列为:");
 		for (int i = 0; i < nums.length; i++) {
 			System.out.print(nums[i] + " ");
 		}
+		System.out.println();
 	}
+
 }
--- Sorting Alogrithm/test/TestSort.java
+++ Sorting Alogrithm/test/TestSort.java
@@ -5,59 +5,71 @@ import sorting.*;
 import java.util.Arrays;
 
 /**
- * Writer: codesky
- * Time: 2017/7/30
  * Description: 测试排序算法.
  */
 public class TestSort {
-    final static int MAX = 200000;
-    final static int RANGE = 100000;
+    final static int MAX = 50000;
+    final static int RANGE = 100;
 
     public static void main(String[] args) {
-        int[] array, array1, array2, array3,array4,array5,array6;
+        int[] array, array11,array12,array13, array2, array3,array4,array5,array6;
         array = RandomArray.getRandomArray(MAX, RANGE);
-        array1 = Arrays.copyOf(array, array.length);
+        array11 = Arrays.copyOf(array, array.length);
+        array12 = Arrays.copyOf(array, array.length);
+        array13 = Arrays.copyOf(array, array.length);
         array2 = Arrays.copyOf(array, array.length);
         array3 = Arrays.copyOf(array, array.length);
         array4 = Arrays.copyOf(array, array.length);
         array5 = Arrays.copyOf(array, array.length);
         array6 = Arrays.copyOf(array, array.length);
 
-        //冒泡排序
-        long start1 = System.currentTimeMillis();
-        BubbleSort.BubbleSortImprove(array1);
+        // 1.1、冒泡排序
+        long start11 = System.currentTimeMillis();
+        BubbleSort.bubbleSort(array11);
         long end1 = System.currentTimeMillis();
-        System.out.println("1.BubbleSort time cost:"+(end1-start1)+"ms");
+        System.out.println("1.1、BubbleSort time cost:"+(end1-start11)+"ms");
+
+        //1.2、冒泡排序
+        long start12 = System.currentTimeMillis();
+        BubbleSort.BubbleSortImprove(array12);
+        end1 = System.currentTimeMillis();
+        System.out.println("1.2、BubbleSort time cost:"+(end1-start12)+"ms");
+
+        //1.3、冒泡排序
+        long start13 = System.currentTimeMillis();
+        BubbleSort.sort(array13);
+        end1 = System.currentTimeMillis();
+        System.out.println("1.3、BubbleSort time cost:"+(end1-start13)+"ms");
 
         //快速排序
-        long start2 = System.currentTimeMillis();
-        QuickSort.quickSort(array2);
-        long end2 = System.currentTimeMillis();
-        System.out.println("2.QuickSort time cost:"+(end2-start2)+"ms");
-
-        //选择排序
-        long start3 = System.currentTimeMillis();
-        SelectionSort.selectionSort(array3);
-        long end3 = System.currentTimeMillis();
-        System.out.println("3.SelectSort time cost:" + (end3 - start3) + "ms");
-
-        //堆排序
-        long start4 = System.currentTimeMillis();
-        HeapSort.heapSort(array4);
-        long end4 = System.currentTimeMillis();
-        System.out.println("4.HeapSort time cost:" + (end4 - start4) + "ms");
-
-        //插入排序
-        long start5 = System.currentTimeMillis();
-        InsertionSort.insertionSort(array5);
-        long end5 = System.currentTimeMillis();
-        System.out.println("5.InsertSort time cost:" + (end5 - start5) + "ms");
-
-        //归并排序
-        long start6 = System.currentTimeMillis();
-        MergeSort.mergeSort(array6);
-        long end6 = System.currentTimeMillis();
-        System.out.println("6.MergeSort time cost:" + (end6 - start6) + "ms");
+//        long start2 = System.currentTimeMillis();
+//        QuickSort.quickSort(array2);
+//        long end2 = System.currentTimeMillis();
+//        System.out.println("2.QuickSort time cost:"+(end2-start2)+"ms");
+//
+//        //选择排序
+//        long start3 = System.currentTimeMillis();
+//        SelectionSort.selectionSort(array3);
+//        long end3 = System.currentTimeMillis();
+//        System.out.println("3.SelectSort time cost:" + (end3 - start3) + "ms");
+//
+//        //堆排序
+//        long start4 = System.currentTimeMillis();
+//        HeapSort.heapSort(array4);
+//        long end4 = System.currentTimeMillis();
+//        System.out.println("4.HeapSort time cost:" + (end4 - start4) + "ms");
+//
+//        //插入排序
+//        long start5 = System.currentTimeMillis();
+//        InsertionSort.insertionSort(array5);
+//        long end5 = System.currentTimeMillis();
+//        System.out.println("5.InsertSort time cost:" + (end5 - start5) + "ms");
+//
+//        //归并排序
+//        long start6 = System.currentTimeMillis();
+//        MergeSort.mergeSort(array6);
+//        long end6 = System.currentTimeMillis();
+//        System.out.println("6.MergeSort time cost:" + (end6 - start6) + "ms");
 
     }
 }
